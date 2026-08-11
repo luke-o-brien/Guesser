6d9cd17d91bcedc08e99c19a384978d84d2d9b6d
luke-obrien <lukeobrien02@gmail.com>
2026-08-11 19:04:22 +0100

    remove unneeded code from previous iteration and app.tsx updated to account for next stage of work

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
 import { Game } from './assets/Components/Game/Game'
-import { InfoPanel } from './assets/Components/InfoPanel/InfoPanel'
 
 function App() {
 
   return (
     <>
-      {/* <InfoPanel /> */}
       <Game />
     </>
   );
